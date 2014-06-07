9c8fcffee5bbebfed543e7c37def2ba9e12bce15
Ruggero <ruggero.bonanno@gmail.com>
2014-06-07 16:50:51 +0200

    aggiunzione commenti al file sudp

--- try/s_udp.c
+++ try/s_udp.c
@@ -9,6 +9,7 @@
 
 int main (int argc, char*  argv[])
 {
+	//dichiaro valori base 
 	int n;
 	int portno, clilen;
 	int sockfd;
@@ -16,6 +17,7 @@ int main (int argc, char*  argv[])
 	struct sockaddr_in a_server, a_client;
 	char buffer[1000];
 
+	//inizializzo socket server + controllo
 	sockfd = socket(AF_INET,SOCK_DGRAM, 0);
 
 	if(sockfd < 0)
@@ -23,18 +25,22 @@ int main (int argc, char*  argv[])
 		printf("ERROR opening socket");
 	}
 	
+	//inizializzo alcuni valori del server dopo aver azzerato i dati precedenti nella struttura
 	bzero((char*)&a_server, sizeof(a_server));
 	portno = atoi(argv[1]);
 	a_server.sin_family = AF_INET;
 	a_server.sin_addr.s_addr = INADDR_ANY;
 	a_server.sin_port = htons(portno);
 	
+	//apro la porta controllando che tutto vada bene
 	n = bind(sockfd, (struct sockaddr*)&a_server, sizeof(a_server));
 
-	printf ("%d\n", n);
+	if (n < 0 )
+		printf ("ERRORE nel binding\n");
 
 	clilen = sizeof(a_client);
 
+	//ciclo ricezione
 	for(;;)
 	{
 		printf("ciao\n");
@@ -43,6 +49,7 @@ int main (int argc, char*  argv[])
 		printf("sono prima\n");
 
 		bytesread = recvfrom(sockfd, buffer, 1000, 0, (struct sockaddr*)&a_client, (socklen_t*)&clilen);
+		//non arriva oltre la recvfrom, non stmapa nulla...
 	
 		printf("sono dopo\n");	
 	
